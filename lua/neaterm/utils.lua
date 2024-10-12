87f3e64e7bb1fbe307b3e1b9ffac292fe59150c3
Dan7h3x <m.jalili.barbin@gmail.com>
2024-10-12 11:08:38 +0330

    Fix: coloring border.

--- lua/neaterm/utils.lua
+++ lua/neaterm/utils.lua
@@ -15,8 +15,6 @@ function M.create_window(opts, term_opts, buf)
     win_opts.height = math.floor(vim.o.lines * opts.float_height)
     win_opts.row = vim.o.lines - win_opts.height - 4
     win_opts.col = math.floor((vim.o.columns - win_opts.width) / 2)
-    win_opts.winhighlight = "NormalFloat:NeatermNormal,FloatBorder:NeatermBorder"
-
     return api.nvim_open_win(buf, true, win_opts)
   elseif term_opts.type == 'full' then
     vim.cmd('enew')
