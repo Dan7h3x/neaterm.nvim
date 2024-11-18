eb3917e773362dc79a948e7706a34b5ec8ebe7af
Dan7h3x <m.jalili.barbin@gmail.com>
2024-11-18 09:51:01 +0330

    updated functions

--- lua/neaterm/config.lua
+++ lua/neaterm/config.lua
@@ -19,10 +19,22 @@ local default_opts = {
     },
   },
   keymaps = {
-    toggle = '<A-t>',
-    new_vertical = '<C-\\>',
-    new_horizontal = '<C-.>',
-    new_float = '<C-A-t>',
+    toggle = {
+      key = '<A-t>',
+      desc = 'Toggle terminal window'
+    },
+    new_vertical = {
+      key = '<C-\\>',
+      desc = 'Create new vertical terminal'
+    },
+    new_horizontal = {
+      key = '<C-.>',
+      desc = 'Create new horizontal terminal'
+    },
+    new_float = {
+      key = '<C-A-t>',
+      desc = 'Create new floating terminal'
+    },
     close = '<C-d>',
     next = '<C-PageDown>',
     prev = '<C-PageUp>',
@@ -102,11 +114,98 @@ local default_opts = {
       get_variables_cmd = "Object.keys(global)",
       exit_cmd = ".exit",
     },
+    sh = {
+      name = "Shell",
+      cmd = vim.o.shell,
+      startup_cmds = {
+        "PS1='$ '",  -- Set simple prompt
+        "TERM=xterm-256color",
+      },
+      get_variables_cmd = "set",  -- List all shell variables
+      inspect_variable_cmd = "echo $",  -- Will be concatenated with variable name
+      exit_cmd = "exit",
+      parse_output = function(output)
+        local vars = {}
+        for line in output:gmatch("[^\r\n]+") do
+          local name, value = line:match("^([%w_]+)=(.+)$")
+          if name then
+            table.insert(vars, {
+              name = name,
+              type = "variable",
+              value = value,
+              display = string.format("%-30s │ %-20s │ %s", name, "shell var", value:sub(1, 30))
+            })
+          end
+        end
+        return vars
+      end
+    },
+    bash = {
+      name = "Bash",
+      cmd = "bash",
+      startup_cmds = {
+        "PS1='\\w $ '",  -- Set prompt with working directory
+        "shopt -s checkwinsize",  -- Update LINES and COLUMNS
+      },
+      get_variables_cmd = "declare -p",  -- List all variables with types
+      inspect_variable_cmd = "declare -p ",  -- Show variable declaration
+      exit_cmd = "exit",
+      parse_output = function(output)
+        local vars = {}
+        for line in output:gmatch("[^\r\n]+") do
+          local decl_type, name = line:match("^declare%s+%-([a-zA-Z])%s+([%w_]+)")
+          if name then
+            local var_type = ({
+              a = "array",
+              A = "associative array",
+              i = "integer",
+              x = "export",
+              r = "readonly",
+              n = "reference",
+            })[decl_type] or "variable"
+            
+            table.insert(vars, {
+              name = name,
+              type = var_type,
+              display = string.format("%-30s │ %-20s", name, var_type)
+            })
+          end
+        end
+        return vars
+      end
+    }
   },
 }
 
 function M.setup(user_opts)
-  return vim.tbl_deep_extend("force", default_opts, user_opts or {})
+  local opts = vim.deepcopy(default_opts)
+  
+  if user_opts then
+    -- Merge repl_configs separately to preserve defaults
+    local user_repl_configs = user_opts.repl_configs
+    user_opts.repl_configs = nil
+    
+    -- Merge main options
+    opts = vim.tbl_deep_extend("force", opts, user_opts)
+    
+    -- Merge REPL configs if provided
+    if user_repl_configs then
+      for ft, config in pairs(user_repl_configs) do
+        if opts.repl_configs[ft] then
+          -- Merge with existing config
+          opts.repl_configs[ft] = vim.tbl_deep_extend("force", 
+            opts.repl_configs[ft], 
+            config
+          )
+        else
+          -- Add new config
+          opts.repl_configs[ft] = config
+        end
+      end
+    end
+  end
+  
+  return opts
 end
 
 return M
