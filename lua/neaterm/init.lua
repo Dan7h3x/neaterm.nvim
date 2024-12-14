d531e433282460878d53236fc40742711c078062
Dan7h3x <m.jalili.barbin@gmail.com>
2024-12-14 18:41:11 +0330

    --wip-- [skip ci]

--- lua/neaterm/init.lua
+++ lua/neaterm/init.lua
@@ -11,12 +11,12 @@ function M.setup(user_opts)
   neaterm:setup_terminal()
   neaterm:setup_keymaps()
   neaterm:setup_repl_completion()
-  neaterm:setup_plot_viewer()
-  neaterm:setup_debug_integration()
+  -- neaterm:setup_plot_viewer()
+  -- neaterm:setup_debug_integration()
   neaterm:setup_workspace_management()
   neaterm:setup_package_manager()
   neaterm:setup_snippets()
-  neaterm:setup_doc_viewer()
+  -- neaterm:setup_doc_viewer()
 
   return neaterm
 end
